d003ce3ce35d7a0763649e33771f15acb86eb67d
ruchisingh111 <startfrominitial@hmail.com>
2025-12-31 14:31:11 +0530

    My Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
